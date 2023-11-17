4475bb7edcef8e7fee9f52b4a3cb50ffd98e47ce
Amit0841 <aroy16063@gmail.com>
2023-11-17 21:15:17 +0530

    Done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
     production: true,
     firebaseConfig: {
-           
+            apiKey: "AIzaSyCZjvGElYHNMxSjd4oNQ1XBhD4GzWULokA",
+            authDomain: "todolist-f9d4e.firebaseapp.com",
+            projectId: "todolist-f9d4e",
+            storageBucket: "todolist-f9d4e.appspot.com",
+            messagingSenderId: "716888141858",
+            appId: "1:716888141858:web:ff9636e5b80cecf04eda7b",
+            measurementId: "G-4T0XKL4LJ3"
     }
   };
